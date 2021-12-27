b1918c0c133be9c1076b05027883048431eee428
superflyman90 <superflyman90@gmail.com>
2021-12-27 09:28:46 +0100

    mis en place de sass et du header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import './app.scss';
 
 class App extends Component {
   render() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,12 @@ const Header = () => {
     return (
         <header>
             <h1>Mon CV en ligne</h1>
+            <nav>
+                <li><a href='#'>Menu 1</a></li>
+                <li><a href='#'>Menu 2</a></li>
+                <li><a href='#'>Menu 3</a></li>
+                <h2>En perpétuelle quête de savoir</h2>
+            </nav>
         </header>
     );
 };
